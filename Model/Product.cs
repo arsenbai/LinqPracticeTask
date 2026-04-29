0978c686e48e400eafd6b65da9ccff80a3c393b8
a.bayseupov <a.bayseupov@a1qa.com>
2026-04-30 00:13:45 +0500

    initial commit

---
+++ Model/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace LinqPracticeTask.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+
+        public Product(int id, string name, string category, decimal price, bool inStock)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+            Price = price;
+            InStock = inStock;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using LinqPracticeTask.Model;
+
+List<Product> products = new List<Product>() {};
+
+List<string> names = new List<string>() { "PC", "Earphones", "Laptop", "Captain America", "Batman", "Joker", "Batman", "Mario", "Hat", "Handwatches", "Gloves", "Mario" };
+List<string> categories = new List<string>() { "Electronics", "Electronics", "Electronics", "Toys", "Toys", "Toys", "Game", "Game", "Accessories", "Accessories", "Accessories", "Beverage" };
+List<decimal> prices = new List<decimal>() { 1227.99m, 200m, 1999m, 44m, 54m, 88m, 70m, 65m, 30m, 300m, 50m, 0.99m };
+List<bool> inStockParamList = new List<bool>() { true, false, true, true, false, true, false, true, true, false, true, true };
+
+for (int i = 0; i < names.Count; i++)
+{
+    products.Add(new Product(i + 1, names[i], categories[i], prices[i], inStockParamList[i]));
+}
+
+//1.Get names of products cheaper than 100.
+var productsCheaperThanHundred = products.FindAll(p => p.Price < 100m).Select(p => p.Name);
+
+//foreach (var item in productsCheaperThanHundred)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//2. Get top 5 most expensive products.
+var topFiveMostExpensiveProducts = products.OrderByDescending(p => p.Price).Take(5);
+
+//foreach (var item in topFiveMostExpensiveProducts)
+//{
+//    Console.WriteLine(item.Name);
+//}
+
+
+//3. Group products by category and return category with its items.
+var groupOfProducts = products.GroupBy(p => p.Category);
+
+//foreach (var group in groupOfProducts)
+//{
+//    Console.WriteLine($"GROUP: {group.Key}");
+//    foreach (var product in group)
+//    {
+//        Console.WriteLine($" - PRODUCT: {product.Name}");
+//    }
+//    Console.WriteLine("---------------");
+//}
+
+
+//4. Find duplicate product names.
+var duplicateProductNames = products.GroupBy(p => p.Name).Where(g => g.Count() > 1).Select(g => g.Key);
+
+//Console.WriteLine("Duplicates:");
+//foreach (var (index, duplicateName) in duplicateProductNames.Index())
+//{
+//    Console.WriteLine($"index({index}): - {duplicateName}");
+//}
+
+
+//5. Get products in 'Electronics' category with price > 500 and in stock.
+var filteredProducts = products.Where(p => p.Category == "Electronics" && p.Price > 500);
+
+//foreach (var electronicProdCheaperThanFiveHundred in filteredProducts)
+//{
+//    Console.WriteLine(electronicProdCheaperThanFiveHundred.Name);
+//}
+
+
+//6. Get a product by Id safely. Consider what happens if it does not exist.
+var productItem1 = products.FirstOrDefault(p => p.Id == 2);
+var productItem2 = products.FirstOrDefault(p => p.Id == 0);
+
+//if (productItem1 is not null)
+//{
+//    Console.WriteLine(productItem1.Name);
+//}
+//else
+//{
+//    Console.WriteLine("PRODUCT NOT FOUND.");
+//}
+
+//if (productItem2 is not null)
+//{
+//    Console.WriteLine(productItem2.Name);
+//}
+//else
+//{
+//    Console.WriteLine("PRODUCT NOT FOUND.");
+//}
+
+
+
+//7. Convert product list into a dictionary using Id as key.
+var productDict = products.ToDictionary(p => p.Id);
+
+//foreach (var item in productDict)
+//{
+//    Console.Write($"KEY: {item.Key} | ");
+//    Console.WriteLine($"VALUE: {item.Value.Name}");
+//}
+
+
+//8.Find top 3 categories by average product price.
+
+var topThreeCategoriesByAvgProductPrice = products
+    .GroupBy(p => p.Category)
+    .Select(g => new { Category = g.Key, AvgPrice = g.Average(p => p.Price) })
+    .OrderByDescending(i => i.AvgPrice)
+    .Take(3);
+
+//foreach (var item in topThreeCategoriesByAvgProductPrice)
+//{
+//    Console.Write($"CATEG: {item.Category} | AvgPrice=");
+//    Console.WriteLine(item.AvgPrice);
+//}
